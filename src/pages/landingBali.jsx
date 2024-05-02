df169842ad8c9f6df362fd73aedb6879b7e63168
emmanuel <eavilesp@aeromexico.com>
2024-05-02 16:47:45 -0600

    cambios de landing

--- src/pages/landingBali.jsx
+++ src/pages/landingBali.jsx
@@ -55,35 +55,12 @@ const influncers = [
             <p className=' text-xl lg:text-base text-center'>¿Estás listo para vivir la aventura de tu vida con Destiny Travel? Únete a nuestra emocionante dinámica y gana la oportunidad de explorar el paraíso de Bali en compañia de tu Influencer favorito.</p>
             <p className=' text-xl text-center mt-4 font-extrabold'> ¡Tu destino de ensueño te espera!</p>
           </div>
-          <div className='my-20 lg:mb-5'>
-            <h2 className='text-3xl font-semibold'>Acompaña a tus influencers:</h2>
-            <div className='py-5 flex flex-col items-center lg:flex-row'>
-              <div className='grid grid-cols-1 gap-2 md:grid-cols-2 lg:grid-cols-5 w-full'>
-                {
-                  influncers?.map( item => (
-                    <a href={ item.instagram }>
-                      <div className='group relative overflow-hidden rounded-xl max-h-52'>
-                        <img className='object-cover h-full w-full' src={ item.img } alt={ item.nombre } />
-                        <div className='bottom-0 left-0 absolute w-full h-full bg-gradient-to-t from-[#010417] from-0%'></div>
-                        <div className='p-2 text-white flex flex-col justify-end bottom-0 left-0 absolute h-full w-full'>
-                          <a target=' blank' href={ item.instagram }>
-                            <p className='font-extrabold'>{ item.nombre }</p>
-                            <p className='text-[#ffd603]'> <i class="fa-brands fa-instagram"></i> { item.nickname }</p>
-                          </a>
-                        </div>
-                      </div>
-                    </a>
-                  ) )
-                }
-              </div>
-            </div>
-          </div>
           <div className='mb-10'>
              <p className='text-2xl text-[#ffd603] font-semibold'> Sumérgete en la etapa 1 siguiendo estos simples pasos: </p> 
              <ul className='px-5 mt-5'>
               <li className='mb-4'>1.Sigue a @experience.destiny en todas sus redes: <a href="https://www.facebook.com/experience.destiny" className='text-[#ffd603]'>Facebook</a>, <a className='text-[#ffd603]' href="https://www.instagram.com/experience.destiny/">Instagram</a> y <a className='text-[#ffd603]' href="https://www.tiktok.com/@experience.destiny"> TikTok</a>. </li> 
               <li className='mb-4'>2.Dale like al video destacado de la dinámica en <a href="https://www.instagram.com/p/C5y45jMxx_Q/" className='text-[#ffd603]'>Instagram</a>.</li>
-              <li className='mb-4'>3.Deja un comentario en el video explicando "¿Por qué quiero ir a Bali?" utilizando el hashtag #ViveLaExperienciaDestiny y etiquetando al creador de contenido.</li>
+              <li className='mb-4'>3.Deja un comentario en el video explicando "¿Por qué quiero ir a Bali?" utilizando el hashtag #ViveLaExperienciaDestiny.</li>
               <li className='mb-4'>4.Encuentra las palabras ocultas en los próximos videos en las redes de Destiny Travel para armar la frase ganadora.</li>
              </ul>
           </div>
